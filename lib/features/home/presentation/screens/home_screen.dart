cfc9c86b703463567ceae903a780e5dd40e1d1a9
Imran Babuji <imranbabuji162002@gmail.com>
2025-12-25 23:50:36 +0530

    FEAT: Finished working with sliver app bar

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -19,37 +19,39 @@ class HomeScreen extends StatelessWidget {
     final itemWidth = (size.width - 20 * 2 - 16) / 2;
     final itemHeight = itemWidth / 0.76;
 
-    return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            // ================= SLIVER APP BAR =================
-            SliverAppBar(
-              expandedHeight: size.height * 0.3,
-              pinned: false,
-              floating: false,
-              automaticallyImplyLeading: false,
-              backgroundColor: GreetingUtils.getBackgroundColor(
-                morningColor: Colors.orange.shade100,
-                nightColor: Colors.black,
-              ),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Lottie.asset(
-                  GreetingUtils.getLottieAsset(
-                    morningLottie: AppAssetsConstants.morningLottie,
-                    nightLottie: AppAssetsConstants.nightLottie,
-                  ),
-                  fit: BoxFit.cover,
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          // ================= SLIVER APP BAR =================
+          SliverAppBar(
+            expandedHeight: size.height * 0.3,
+            pinned: false,
+            floating: false,
+            automaticallyImplyLeading: false,
+            backgroundColor: GreetingUtils.getBackgroundColor(
+              morningColor: Colors.orange.shade100,
+              nightColor: Colors.black,
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Lottie.asset(
+                GreetingUtils.getLottieAsset(
+                  morningLottie: AppAssetsConstants.morningLottie,
+                  nightLottie: AppAssetsConstants.nightLottie,
                 ),
+                fit: BoxFit.cover,
               ),
             ),
+          ),
 
-            // ================= PINNED GREETING HEADER =================
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: GreetingHeaderDelegate(
-                height: 110,
+          // ================= PINNED GREETING HEADER =================
+          SliverPersistentHeader(
+            pinned: true,
+
+            delegate: GreetingHeaderDelegate(
+              height: 110,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child:
                     Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -77,101 +79,101 @@ class HomeScreen extends StatelessWidget {
                         .slideY(begin: -0.2, end: 0),
               ),
             ),
+          ),
 
-            // ================= DAILY MEDITATION CARD =================
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      child: SvgPicture.asset(
-                        AppAssetsConstants.dailyMeditate,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
+          // ================= DAILY MEDITATION CARD =================
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              child: Stack(
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: SvgPicture.asset(
+                      AppAssetsConstants.dailyMeditate,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
                     ),
-                    Positioned.fill(
-                      child: GestureDetector(
-                        onTap: () {
-                          HapticFeedback.heavyImpact();
-                          GoRouter.of(
-                            context,
-                          ).pushNamed(AppRouterConstants.audio);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 20,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  KText(
-                                    text: "Daily Meditation",
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.w800,
-                                    color: AppColors.bgColor,
-                                  ),
-                                  KText(
-                                    text: "Meditations 3-10 MINS",
-                                    fontSize: 14,
-                                    color: AppColors.bgColor,
-                                  ),
-                                ],
-                              ),
-                              Icon(
-                                Icons.play_circle_fill_outlined,
-                                size: 42,
-                                color: AppColors.bgColor,
-                              ),
-                            ],
-                          ),
+                  ),
+                  Positioned.fill(
+                    child: GestureDetector(
+                      onTap: () {
+                        HapticFeedback.heavyImpact();
+                        GoRouter.of(
+                          context,
+                        ).pushNamed(AppRouterConstants.audio);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 20,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                KText(
+                                  text: "Daily Meditation",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w800,
+                                  color: AppColors.bgColor,
+                                ),
+                                KText(
+                                  text: "Meditations 3-10 MINS",
+                                  fontSize: 14,
+                                  color: AppColors.bgColor,
+                                ),
+                              ],
+                            ),
+                            Icon(
+                              Icons.play_circle_fill_outlined,
+                              size: 42,
+                              color: AppColors.bgColor,
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  ],
-                ).animate().fadeIn(duration: 700.ms).slideY(begin: 0.3, end: 0),
+                  ),
+                ],
+              ).animate().fadeIn(duration: 700.ms).slideY(begin: 0.3, end: 0),
+            ),
+          ),
+
+          // ================= SLIVER GRID =================
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            sliver: SliverGrid(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return MeditateAudioCard(
+                      audioTitle: "Relaxation Music with Candle Love Move",
+                      onTap: () {
+                        GoRouter.of(
+                          context,
+                        ).pushNamed(AppRouterConstants.audio);
+                      },
+                    )
+                    .animate()
+                    .fadeIn(duration: 200.ms, delay: (index * 80).ms)
+                    .slideY(begin: 0.3, end: 0);
+              }, childCount: 20),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: itemWidth / itemHeight,
               ),
             ),
+          ),
 
-            // ================= SLIVER GRID =================
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              sliver: SliverGrid(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return MeditateAudioCard(
-                        audioTitle: "Relaxation Music with Candle Love Move",
-                        onTap: () {
-                          GoRouter.of(
-                            context,
-                          ).pushNamed(AppRouterConstants.audio);
-                        },
-                      )
-                      .animate()
-                      .fadeIn(duration: 500.ms, delay: (index * 80).ms)
-                      .slideY(begin: 0.3, end: 0);
-                }, childCount: 20),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  childAspectRatio: itemWidth / itemHeight,
-                ),
-              ),
-            ),
-
-            const SliverToBoxAdapter(child: SizedBox(height: 30)),
-          ],
-        ),
+          const SliverToBoxAdapter(child: SizedBox(height: 30)),
+        ],
       ),
     );
   }
